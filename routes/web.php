d37a1492e8b7280a37808c6e4c126700a5edef8e
HafidhAsyi <hafidhasyisalahuddin@gmail.com>
2025-02-19 17:37:52 +0700

    tambah route, views, tailwindcss, alpinejs

--- routes/web.php
+++ routes/web.php
@@ -3,18 +3,38 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('homepage');
+    return view('homePage');
 });
 
-Route::get('/profil', function () {
-    return view('profilStaf', ['nama' => 'Hafidh Asyi']);
+Route::get('/category', function () {
+    return view('categoryPage');
 });
 
-Route::get('/profil', function () {
-    return view('profilStaf', ['nama' => 'Hafidh Asyi']);
+Route::get('/year', function () {
+    return view('yearPage');
 });
 
-Route::get('/profil', function () {
-    return view('profilStaf', ['nama' => 'Hafidh Asyi']);
+Route::get('/detail', function () {
+    return view('detailPage');
+});
+
+Route::get('/alarmHistory', function () {
+    return view('alarmHistoryPage');
+});
+
+Route::get('/reportHistory', function () {
+    return view('reportHistoryPage');
+});
+
+Route::get('/edit', function () {
+    return view('editPage');
+});
+
+Route::get('/add', function () {
+    return view('addPage');
+});
+
+Route::get('/welcome', function () {
+    return view('wellcome');
 });
 
